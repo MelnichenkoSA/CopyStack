39a349dc62e160616c1e7cd8cf8395afd277462f
MelnichenkoSA <112738977+MelnichenkoSA@users.noreply.github.com>
2023-10-03 14:41:41 +0700

    Update Program.cs

--- CopyStack/Program.cs
+++ CopyStack/Program.cs
@@ -1,6 +1,9 @@
 ﻿using CopyStack;
 
-LinkedListStack stack = new LinkedListStack();
+LinkedListStack stack = new LinkedListStack(5);
+stack.Pop();
+
+stack.Sred();
 
 stack.Push(1);
 stack.Push(2);
@@ -8,9 +11,13 @@ stack.Push(3);
 stack.Push(4);
 stack.Push(5);
 
+Console.WriteLine("Среднее:");
+Console.WriteLine(stack.Sred());
+
+stack.Push(6);
 Console.WriteLine("Вершина стека: " + stack.Peek());
 
-LinkedListStack copyStack = stack.Copy();
+LinkedListStack copyStack = new LinkedListStack(stack, stack.max);
 
 Console.WriteLine("Стэк:");
 while (!stack.IsEmpty())
